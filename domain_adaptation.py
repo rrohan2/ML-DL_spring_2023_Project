0de789223a34c6e035871c901cdc06b12325ec35
vshivak1 <vshivak1@login01.rockfish.cluster>
2023-05-07 23:05:26 -0400

    adda style domain adaptation using resnets implemented

--- domain_adaptation.py
+++ domain_adaptation.py
@@ -12,52 +12,88 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # get test dataset path....
 class Classifier(nn.Module):
-    def __init__(self, nin, num_classes):
+    def __init__(self, num_classes, final_layer):
         super(Classifier, self).__init__()
-        self.classifier = nn.Sequential(
-            nn.Linear(nin, num_classes)
-        )
-
+        self.features = models.resnet50(pretrained=True)
+        self.classifier = nn.Linear(self.features.fc.in_features, num_classes)
+        self.classifier.load_state_dict(final_layer)
     def forward(self, x):
+        x = self.features(x)
         x = self.classifier(x)
         return x
 
 # Define the adversarial domain adaptation training function
-def train_model(model, source_loader, target_loader, optimizer, criterion, alpha):
-    model.train()
-
+def train_model(epoch, discriminator, target_model, source_model, target_classifier, source_loader, target_loader, 
+                disc_optimizer, target_optimizer, disc_criterion, target_criterion):
     for batch_idx, (source_data, source_target) in enumerate(source_loader):
         try:
             target_data, target_target = next(target_iter)
         except:
             target_iter = iter(target_loader)
             target_data, target_target = next(target_iter)
-            
-        optimizer.zero_grad()
+        
+        # discriminator training
+        target_model.eval()
+        discriminator.train()
 
+        target_data, source_data = target_data.to(device), source_data.to(device)
+        
         # Forward pass on the source data
-        source_pred = model(source_data)
-        source_loss = criterion(source_pred, source_target)
-
+        source_pred = source_model(source_data)
         # Forward pass on the target data
-        target_pred = model(target_data)
-        target_loss = criterion(target_pred, target_target)
+        target_pred = target_model(target_data)
 
-        # Compute the domain confusion loss
-        source_features = model.features(source_data)
-        target_features = model.features(target_data)
-        domain_loss = alpha * criterion(source_features.mean(0) - target_features.mean(0), torch.zeros_like(source_features.mean(0)))
+        # Setup discriminator task
+        source_features = source_pred.view(source_pred.shape[0], -1)
+        target_features = target_pred.view(target_pred.shape[0], -1)
+        features = torch.cat([source_features, target_features])
+        true_labels = torch.cat([torch.ones(source_features.shape[0]),
+                                 torch.zeros(target_features.shape[0])])
+        features, true_labels = features.to(device), true_labels.to(device)
+        loss = disc_criterion(discriminator(features).squeeze(), true_labels)
 
         # Backward pass and optimization
-        total_loss = source_loss + target_loss + domain_loss
-        total_loss.backward()
-        optimizer.step()
+        disc_optimizer.zero_grad()
+        loss.backward()
+        disc_optimizer.step()
 
+        ## above trains the discriminator to tell if a given feature tensor 
+        ## is from the source or target model
+
+
+        ### Now train the target classifier to be better at fooling the discriminator
+        target_model.train()
+        discriminator.eval()
+
+        target_pred = target_model(target_data)
+        features = target_pred.view(target_pred.shape[0], -1)
+        true_labels = torch.cat([torch.ones(features.shape[0])])
+        features, true_labels = features.to(device), true_labels.to(device)
+        loss = disc_criterion(discriminator(features).squeeze(), true_labels)
+
+        # Backward pass and optimization
+        target_optimizer.zero_grad()
+        loss.backward()
+        target_optimizer.step()
+
+        ## above trains the target model to produce features that 
+        ## more closely resemble that of the source model
+
+        target_classifier.eval()
+        if batch_idx % 10 == 0:
+            outputs = target_classifier(target_data)
+            _, predicted = torch.max(outputs.data, 1)
+            total += target_target.size(0)
+            correct += (predicted == target_target).sum().item()
+            accuracy = 100 * correct / total
+            print('Test Accuracy: {:.2f}%'.format(accuracy))
+        
         # Print progress
         if batch_idx % 10 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(source_data), len(source_loader.dataset),
-                100. * batch_idx / len(source_loader), total_loss.item()))
+                100. * batch_idx / len(source_loader), loss.item()))
+
 
 # Define the main function
 def main():
@@ -73,23 +109,47 @@ def main():
 
     # Set up the model, optimizer, and criterion
     # Load pre-trained ResNet-50 model
-    model = models.resnet50(pretrained=True)
+    num_classes = 1000 # Number of classes in ImageNet dataset
+    full_undistored_model = models.resnet50(pretrained=True)
+    full_undistored_model.fc = nn.Linear(full_undistored_model.fc.in_features, num_classes)
+    checkpoint_dir = '/content/drive/MyDrive/imagenet-mini/res50model_checkpoint.pth'
+    checkpoint = torch.load(checkpoint_dir)
+    full_undistored_model.load_state_dict(checkpoint)
 
-    # Modify the last fully-connected layer to match the number of classes in ImageNet dataset
-    num_classes = 1000 # Number of classes in ImageNet dataset    
-    model = Classifier(model.fc.in_features, num_classes)
-    model = model.to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    ## same model as above, but now just the feature producer, not the final layer
+    source_model = models.resnet50(pretrained=True)
+    checkpoint = torch.load(checkpoint_dir)
+    source_model.load_state_dict(checkpoint)
+    source_model = source_model.to(device)
+
+    # setup new model based on resnet
+    # model is trained up until final layer (to produce features on distorted images
+    # similar to their undistorted counterparts)
+    # and final layer is undisturbed to classify, as if features were undistorted
+    target_model = Classifier(num_classes, full_undistored_model.fc.state_dict())
+    target_model_features = target_model.features.to(device)
+
+    discriminator = nn.Sequential(
+            nn.Linear(target_model.features.fc.in_features, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1)
+        )
+    discriminator = discriminator.to(device)
+
+    target_optimizer = optim.Adam(target_model.parameters(), lr=0.001)
+    disc_optimizer = optim.Adam(discriminator.parameters(), lr=0.001)
     criterion = nn.CrossEntropyLoss()
+    bce = nn.BCEWithLogitsLoss()
 
     # Set the alpha parameter for the domain confusion loss
     alpha = 0.2 # playing around with this
 
     # Train the model using domain adaptation
     for epoch in range(15): #may change
-        train_model(model, source_loader, target_loader, optimizer, criterion, alpha)
+        train_model(epoch, discriminator, target_model_features, source_model, target_model, source_loader, target_loader, 
+                disc_optimizer, target_optimizer, bce, criterion)
 
     # Evaluate
-    test_loss, test_acc = model.evaluate(x_hold, y_hold)
+    test_loss, test_acc = target_model.evaluate(x_hold, y_hold)
     print('Test accuracy:', test_acc)
 
